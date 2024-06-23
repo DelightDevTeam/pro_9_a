05a8769bd34e4ecefaa664b7e63f477f536ed33b
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-06-23 18:49:20 +0630

    joker game lists updated by amk

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
             HotGameTablesSeeder::class,
             PragmaticPlaySeeder::class,
             PGSoftGameListSeeder::class,
+            JokerGameListSeeder::class,
         ]);
       
     }
