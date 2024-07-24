358b7e9c31a3630fdf4726f7339fa496715a2989
maxwin-sourcecode <maxwinsourcecode@gmail.com>
2024-07-24 08:45:52 +0630

    paymenttype

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,13 +20,13 @@ class UsersTableSeeder extends Seeder
         $admin = $this->createUser(UserType::Admin, 'Owner', 'mk77', '09123456789');
         (new WalletService)->deposit($admin, 10 * 100_000, TransactionName::CapitalDeposit);
 
-        $master = $this->createUser(UserType::Master, 'Master 1', 'TG-898437', '09112345678', $admin->id);
+        $master = $this->createUser(UserType::Master, 'Master 1', 'MK898437', '09112345678', $admin->id);
         (new WalletService)->transfer($admin, $master, 8 * 100_000, TransactionName::CreditTransfer);
 
-        $agent_1 = $this->createUser(UserType::Agent, 'Agent 1', 'TG-898737', '09112345674', $master->id);
+        $agent_1 = $this->createUser(UserType::Agent, 'Agent 1', 'MKA898737', '09112345674', $master->id);
         (new WalletService)->transfer($master, $agent_1, 5 * 100_000, TransactionName::CreditTransfer);
 
-        $player_1 = $this->createUser(UserType::Player, 'Player 1', 'TG-111111', '09111111111', $agent_1->id);
+        $player_1 = $this->createUser(UserType::Player, 'Player 1', 'MKP111111', '09111111111', $agent_1->id);
         (new WalletService)->transfer($agent_1, $player_1, 30000, TransactionName::CreditTransfer);
 
     }
