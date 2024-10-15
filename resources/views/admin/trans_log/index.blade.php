1b9c89aa71c60c0f27191fb1c4b621d53c4a3dab
sophia <thangneimawi@aceplussolutions.com>
2024-10-15 22:42:28 +0700

    add filter

--- resources/views/admin/trans_log/index.blade.php
+++ resources/views/admin/trans_log/index.blade.php
@@ -1,36 +1,105 @@
 @extends('admin_layouts.app')
 @section('styles')
 <style>
-.transparent-btn {
- background: none;
- border: none;
- padding: 0;
- outline: none;
- cursor: pointer;
- box-shadow: none;
- appearance: none;
- /* For some browsers */
-}
+  .transparent-btn {
+    background: none;
+    border: none;
+    padding: 0;
+    outline: none;
+    cursor: pointer;
+    box-shadow: none;
+    appearance: none;
+    /* For some browsers */
+  }
+
+
+  .custom-form-group {
+    margin-bottom: 20px;
+  }
+
+  .custom-form-group label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+    color: #555;
+  }
+
+  .custom-form-group input,
+  .custom-form-group select {
+    width: 100%;
+    padding: 10px 15px;
+    border: 1px solid #e1e1e1;
+    border-radius: 5px;
+    font-size: 16px;
+    color: #333;
+  }
+
+  .custom-form-group input:focus,
+  .custom-form-group select:focus {
+    border-color: #d33a9e;
+    box-shadow: 0 0 5px rgba(211, 58, 158, 0.5);
+  }
+
+  .submit-btn {
+    background-color: #d33a9e;
+    color: white;
+    border: none;
+    padding: 12px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 18px;
+    font-weight: bold;
+  }
+
+  .submit-btn:hover {
+    background-color: #b8328b;
+  }
 </style>
 @endsection
 @section('content')
 <div class="row mt-4">
  <div class="col-12">
   <div class="card">
-   <!-- Card header -->
-   <div class="card-header pb-0">
-    <div class="d-lg-flex">
-     <div>
-      <h5 class="mb-0">Transfer Log</h5>
+  <div class="card-header pb-0">
+        <div class="card-body">
+          <h5 class="mb-0">Deposit Requested Lists</h5>
+          <form action="{{route('admin.transferLog')}}" method="GET">
+            <div class="row mt-4">
+              <div class="col-lg-2">
+                <div class="custom-form-group">
+                  <label for="Status">Type</label>
+                  <select class="form-control" id="" name="type">
+                    <option selected>Select Type</option>
+                    <option value="withdraw" {{ request()->get('type') == 'withdraw' ? 'selected' : ''  }}>Deposit
+                    </option>
+                    <option value="deposit" {{ request()->get('type') == 'deposit' ? 'selected' : ''  }}>Withdraw
+                    </option>
+                  
+                  </select>
+                </div>
+              </div>
+              <div class="col-lg-2">
+                <div class="custom-form-group">
+                  <label for="Start Date">Start Date</label>
+                  <input type="date" class="form-control" id="start_date" name="start_date" value="{{request()->start_date}}">
+                </div>
+              </div>
+              <div class="col-lg-2">
+                <div class="custom-form-group">
+                  <label for="End Date">End Date</label>
+                  <input type="date" class="form-control" id="end_date" name="end_date" value="{{request()->end_date}}">
+                </div>
+              </div>
 
-     </div>
-     <div class="ms-auto my-auto mt-lg-0 mt-4">
-      <div class="ms-auto my-auto">
+              <div class="col-lg-2 mt-4">
+                <button type="submit" class="btn bg-gradient-primary btn-sm mb-0">Search</button>
+                <a href="{{route('admin.transferLog')}}" class="btn bg-gradient-primary btn-sm mb-0">Refresh</a>
+              </div>
+            </div>
 
+          </form>
+        </div>
       </div>
-     </div>
-    </div>
-   </div>
    <div class="table-responsive">
     <table class="table table-flush" id="users-search">
      <thead class="thead-light">
